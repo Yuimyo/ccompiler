951fd22812a6e112c3e952013a559678b4bd7a28
Yuimyo <Yuimyo@users.noreply.github.com>
2024-04-13 19:49:09 +0900

    掛け算(*)、割り算(/)、括弧((,))を追加

--- ccomp.c
+++ ccomp.c
@@ -5,6 +5,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+//
+// トークナイザー
+//
+
 // トークンの種類
 typedef enum
 {
@@ -124,7 +128,7 @@ Token *tokenize(char *p)
 			continue;
 		}
 
-		if (*p == '+' || *p == '-')
+		if (strchr("+-*/()", *p))
 		{
 			cur = new_token(TK_RESERVED, cur, p++);
 			continue;
@@ -137,11 +141,131 @@ Token *tokenize(char *p)
 			continue;
 		}
 
-		error_at(p, "トークナイズできません");
+		error_at(p, "不正なトークンです");
 	}
 
 	new_token(TK_EOF, cur, p);
 	return head.next;
+}
+
+//
+// パーサー
+//
+
+typedef enum
+{
+	ND_ADD, // +
+	ND_SUB, // -
+	ND_MUL, // *
+	ND_DIV, // /
+	ND_NUM, // 整数
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node
+{
+	NodeKind kind; // ノードの型
+	Node *lhs;	   // 子(左)
+	Node *rhs;	   // 子(右)
+	int val;	   // kindがND_NUMの場合のみ使う
+};
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs)
+{
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = kind;
+	node->lhs = lhs;
+	node->rhs = rhs;
+	return node;
+}
+
+Node *new_node_num(int val)
+{
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = ND_NUM;
+	node->val = val;
+	return node;
+}
+
+Node *expr();
+Node *mul();
+Node *primary();
+
+// expr = mul ("+" mul | "-" mul)*
+Node *expr()
+{
+	Node *node = mul();
+	for (;;)
+	{
+		if (consume('+'))
+			node = new_node(ND_ADD, node, mul());
+		else if (consume('-'))
+			node = new_node(ND_SUB, node, mul());
+		else
+			return node;
+	}
+}
+
+// mul = primary ("*" primary | "/" primary)*
+Node *mul()
+{
+	Node *node = primary();
+	for (;;)
+	{
+		if (consume('*'))
+			node = new_node(ND_MUL, node, primary());
+		else if (consume('/'))
+			node = new_node(ND_DIV, node, primary());
+		else
+			return node;
+	}
+}
+
+// primary = "(" expr ")" | num
+Node *primary()
+{
+	if (consume('('))
+	{
+		Node *node = expr();
+		expect(')');
+		return node;
+	};
+	return new_node_num(expect_number());
+}
+
+void gen(Node *node)
+{
+	if (node->kind == ND_NUM)
+	{
+		printf("  push %d\n", node->val);
+		return;
+	}
+
+	gen(node->lhs);
+	gen(node->rhs);
+
+	printf("  pop rdi\n");
+	printf("  pop rax\n");
+
+	switch (node->kind)
+	{
+	case ND_ADD:
+		printf("  add rax, rdi\n");
+		break;
+	case ND_SUB:
+		printf("  sub rax, rdi\n");
+		break;
+	case ND_MUL:
+		printf("  imul rax, rdi\n");
+		break;
+	case ND_DIV:
+		printf("  cqo\n");
+		printf("  idiv rdi\n");
+		break;
+	}
+
+	printf("  push rax\n");
 }
 
 int main(int argc, char **argv)
@@ -154,26 +278,32 @@ int main(int argc, char **argv)
 
 	user_input = argv[1];
 	token = tokenize(argv[1]);
+	Node *node = expr();
 
 	printf(".intel_syntax noprefix\n");
 	printf(".globl main\n");
 	printf("main:\n");
 
-	printf("  mov rax, %d\n", expect_number());
-	// `+ <数>`あるいは`- <数>`というトークンの並びを消費しつつ
-	// アセンブリを出力
-	while (!at_eof())
-	{
-		if (consume('+'))
-		{
-			printf("  add rax, %d\n", expect_number());
-			continue;
-		}
+	// printf("  mov rax, %d\n", expect_number());
+	// // `+ <数>`あるいは`- <数>`というトークンの並びを消費しつつ
+	// // アセンブリを出力
+	// while (!at_eof())
+	// {
+	// 	if (consume('+'))
+	// 	{
+	// 		printf("  add rax, %d\n", expect_number());
+	// 		continue;
+	// 	}
 
-		expect('-');
-		printf("  sub rax, %d\n", expect_number());
-	}
+	// 	expect('-');
+	// 	printf("  sub rax, %d\n", expect_number());
+	// }
 
+	gen(node);
+
+	// スタックトップに式全体の値が残っているはずなので
+	// それをRAXにロードして関数からの返り値とする
+	printf("  pop rax\n"); // 電卓の計算結果を格納
 	printf("  ret\n");
 	return 0;
 }
--- test.sh
+++ test.sh
@@ -19,6 +19,10 @@ assert 0 0
 assert 42 42
 assert 21 "5+20-4"
 assert 32 " 15 + 24 - 7 "
+assert 47 "5+6*7"
+assert 35 "10/2+6*5"
+assert 50 "10*(3+2)"
+assert 4 "(3+5)/2"
 
 echo OK
 
