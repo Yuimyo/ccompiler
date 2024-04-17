6907533fe9d0a44da5175a81da7908eb5da82f51
Yuimyo <Yuimyo@users.noreply.github.com>
2024-04-18 08:44:06 +0900

    エラー処理実装を別ファイル(err.c)に分離

---
+++ err.c
@@ -0,0 +1,39 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "ccomp.h"
+
+// エラーを報告するための関数
+// printfと同じ引数を取る
+void error(char *fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
+// 入力プログラム
+char *user_input;
+
+// エラー箇所を報告するための関数
+// 第2引数より、printfと同じ引数を取る
+void error_at(char *loc, char *fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+
+    int pos = loc - user_input;
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "%*s", pos, " "); // pos個の空白を出力
+    fprintf(stderr, "^ ");
+
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+
+    exit(1);
+}
--- parse.c
+++ parse.c
@@ -24,38 +24,6 @@ struct Token
 // 現在着目しているトークン
 Token *token;
 
-// エラーを報告するための関数
-// printfと同じ引数を取る
-void error(char *fmt, ...)
-{
-    va_list ap;
-    va_start(ap, fmt);
-    vfprintf(stderr, fmt, ap);
-    fprintf(stderr, "\n");
-    exit(1);
-}
-
-// 入力プログラム
-char *user_input;
-
-// エラー箇所を報告するための関数
-// 第2引数より、printfと同じ引数を取る
-void error_at(char *loc, char *fmt, ...)
-{
-    va_list ap;
-    va_start(ap, fmt);
-
-    int pos = loc - user_input;
-    fprintf(stderr, "%s\n", user_input);
-    fprintf(stderr, "%*s", pos, " "); // pos個の空白を出力
-    fprintf(stderr, "^ ");
-
-    vfprintf(stderr, fmt, ap);
-    fprintf(stderr, "\n");
-
-    exit(1);
-}
-
 // 次のトークンが期待している記号のときには、トークンを1つ読み進めて
 // 真を返す。それ以外の場合には偽を返す。
 bool consume(char *op)
